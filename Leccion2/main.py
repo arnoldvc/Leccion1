bec2aba965822b9e98857f30cda88bfe31802732
arnoldvc <arnoldvc@gmail.com>
2021-06-08 10:47:03 -0400

    Leccion 03

---
+++ Leccion2/main.py
@@ -0,0 +1,7 @@
+numero1 = int(input('Proporciona el Numero 1: '))
+numero2 = int(input('Proporciona el Numero 2: '))
+
+if numero1 > numero2:
+    print(f'El Numero 1 es mayor que el Numero 2')
+else:
+    print(f'El Numero 2 es mayor que el Numero 1')
